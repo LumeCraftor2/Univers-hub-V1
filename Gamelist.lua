79f49362b751d64e5318cdf9739079f0ed4fb020
LumeCraftor2 <duenasbrenton810@gmail.com>
2025-01-29 14:06:29 +0800

    Update Gamelist.lua

--- Gamelist.lua
+++ Gamelist.lua
@@ -1,6 +1,6 @@
 local Games = {
-  [18901165922] = "https://raw.githubusercontent.com/LumeCraftor2/Univers-hub-V1/refs/heads/main/BeeMasterSimulator", -- Bee Master Simulator
-  [16732694052] = "https://github.com/LumeCraftor2/Univers-hub-V1/blob/main/FishTraining", -- fish Training
+    [18901165922] = "https://raw.githubusercontent.com/LumeCraftor2/Univers-hub-V1/main/BeeMasterSimulator.lua",
+    [16732694052] = "https://raw.githubusercontent.com/LumeCraftor2/Univers-hub-V1/main/FishTraining.lua"
 }
 
 return Games
